14eb9a4f0073d1874ab4a40aec57525eb1733ff4
sashaharmer@yahoo.com <sashaharmer@yahoo.com>
2022-12-25 17:13:59 -0500

    removed src file

--- backend/src/utils/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { getToken } from "./tokenServices";
-
-export default function setAuthToken() {
-    let token = getToken()
-    axios.defaults.headers.common['Authorization'] = (token !== null)? `Bearer ${token}`: ''
-}
